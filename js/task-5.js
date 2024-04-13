6bf4c17cb12284e7ecfce9d8180fbb99475144e2
Burlacova <113798594+Burlacova@users.noreply.github.com>
2024-04-13 19:52:53 +0300

    add hm

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
+
+const myButton = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const baseColor = document.querySelector("body");
+
+myButton.addEventListener("click", doColor);
+
+function doColor(event) {
+	const newColor = `${getRandomHexColor()}`;
+	spanColor.textContent = newColor;
+	baseColor.style.backgroundColor = spanColor.textContent;
+	// spanColor.style.backgroundColor = newColor;
 }
--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,37 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
+
+const inputNumber = document.querySelector('[type="number"]');
+const buttonCreate = document.querySelector("[data-create]");
+const buttonDestroy = document.querySelector("[data-destroy]");
+const mother = document.querySelector("#boxes");
+let size = 30;
+
+buttonCreate.addEventListener("click", createDiv);
+buttonDestroy.addEventListener("click", destroyBoxes);
+
+function createDiv(event) {
+	const amount = inputNumber.value;
+	if (amount >= Number(inputNumber.min) && amount <= Number(inputNumber.max)) {
+		destroyBoxes();
+		for (let i = 0; i < amount; i++) {
+			const color = `${getRandomHexColor()}`;
+			const box = document.createElement("div");
+			box.style.height = `${size}px`;
+			box.style.width = `${size}px`;
+			size = size + 10;
+			box.style.backgroundColor = color;
+			mother.append(box);
+
+			inputNumber.value = "";
+		}
+	}
+}
+
+function destroyBoxes() {
+	mother.textContent = "";
+	size = 30;
 }
